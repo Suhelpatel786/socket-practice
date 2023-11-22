8fc3e5bced2169f0e1907963dd9ae9bf904e0c1d
Suhel Patel <suhel.softcolon@gmail.com>
2023-11-22 21:19:20 +0530

    restrict user to enter bad words /*

--- socket/src/Chat.js
+++ socket/src/Chat.js
@@ -4,13 +4,55 @@ import ScrollToBottom from "react-scroll-to-bottom";
 export default function Chat({ userName, room, socket }) {
   const [currentMessage, setCurrentMessage] = useState("");
   const [messageList, setMessageList] = useState([]);
+  const [rightMessage, setRightMessage] = useState("");
+  const [badWordsArray, setBadWordsArray] = useState([
+    "fuck",
+    "shit",
+    "fucker",
+    "bitch",
+    "pigfucker",
+    "ass",
+    "damn",
+    "jesus",
+    "kike",
+    "cocksucker",
+    "nigra",
+    "wanker",
+    "bullshit",
+    "asshole",
+    "bloody",
+    "cock",
+    "dick",
+    "fatherfucker",
+    "goddamn",
+  ]);
 
   const send_message = async () => {
     if (currentMessage !== "") {
+      let updateMessage = currentMessage;
+      const currentMessageArray = currentMessage.split(" ");
+
+      for (let i = 0; i < badWordsArray.length; i++) {
+        for (let j = 0; j < currentMessageArray.length; j++) {
+          if (currentMessageArray[j] === badWordsArray[i]) {
+            const firstCharacter = currentMessageArray[j].charAt(0);
+            const sensoredChar = "*".repeat(currentMessageArray[j].length - 1);
+
+            const censoredWord = firstCharacter + sensoredChar;
+
+            //the updating current message change logic
+            updateMessage = updateMessage.replace(
+              currentMessageArray[j],
+              censoredWord
+            );
+          }
+        }
+      }
+
       const messageData = {
         room: room,
         author: userName,
-        message: currentMessage,
+        message: updateMessage,
         time:
           new Date(Date.now()).getHours() +
           ":" +
